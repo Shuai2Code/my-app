84d2652944833f59f5ad876eb866df9c9180d6be
CodeCan <137749272@qq.com>
2024-07-29 20:08:51 +0800

    useState-object

--- src/App.js
+++ src/App.js
@@ -1,21 +1,37 @@
 import { useState } from 'react'
 
 const Button = () => {
+  const [stusent, setStudent] = useState({
+    name: 'zhangsan',
+    age: 18
+  })
   const [count, setCount] = useState(0)
   const clickHandler = (name, e) => {
     console.log('button 被点击了', count)
     setCount(count + 1)
   }
+  function changeObject() {
+    setStudent({
+      name: stusent.name + 's',
+      age: stusent.age + 1
+    })
+  }
   return (
-    <button
-      type="button"
-      className="btn btn-default"
-      onClick={(e) => {
-        clickHandler('jack', e)
-      }}
-    >
-      button-{count}
-    </button>
+    <div>
+      <button
+        type="button"
+        className="btn btn-default"
+        onClick={(e) => {
+          clickHandler('jack', e)
+        }}
+      >
+        button-{count}
+      </button>
+      <br />
+      <button onClick={changeObject}>
+        {stusent.name}-{stusent.age}
+      </button>
+    </div>
   )
 }
 
